e4f74374643b8a6ee4bacc1aa487535c8693b61a
Asko <49131864+asko1@users.noreply.github.com>
2025-05-25 03:30:39 +0300

    Item/skins

---
+++ src/scripts/dump_skins.ts
@@ -0,0 +1,15 @@
+import { writeFileSync } from 'node:fs';
+import { getFile } from '../DataParser.js';
+import { meta } from '../Meta.js';
+import { RodSkinsLibrary } from '../types/RodSkins.js';
+import { jsonToLua } from '../util/LuaObject.js';
+
+const rodSkins: RodSkinsLibrary = await getFile('RodSkins.json')
+
+const rodSkinData = {}
+
+for (const [name, rodSkin] of Object.entries(rodSkins.Skins).sort(([b], [b1]) => b.localeCompare(b1))) {
+    rodSkinData[name] = rodSkin.Rarity
+}
+
+writeFileSync('./output/RodSkinData.lua', `-- Last update: Version [[${meta.official_version}]], place version ${meta.place_version}\n\nreturn ` + jsonToLua(rodSkinData))
